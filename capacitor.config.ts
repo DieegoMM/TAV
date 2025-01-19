24ca7d4a6eecfaa47a37c577b4da3546be5f894a
DieegoMM <127214383+DieegoMM@users.noreply.github.com>
2025-01-19 18:02:37 -0300

    permisos e instalacion de capacitor camera

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tav.app',
+  appName: 'tav',
+  webDir: 'www', // Directorio donde Ionic compila los archivos
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAkwor_F0clS1OX-AMdGie_JoHWMC-o0h8",
+    authDomain: "tav-try1.firebaseapp.com",
+    projectId: "tav-try1",
+    storageBucket: "tav-try1.firebasestorage.app",
+    messagingSenderId: "491519742217",
+    appId: "1:491519742217:web:f0d643facdbea77ab203d8",
+    measurementId: "G-DR1J4RFQKT"
+  }
 };
